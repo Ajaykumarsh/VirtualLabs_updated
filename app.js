0da1c5ff04e5d345f1143bbaf8c026ca679592a3
Ajaykumar s Hadimani <ajaykumarsh022@gmail.com>
2021-01-26 18:16:19 +0530

    cn labs

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const session = require("express-session");
 
 const studentRouter = require("./Routes/StudentAuth.js");
 const quiz_solution = require("./Routes/quiz_solution.js");
-const adminRouter=require('./Routes/adminAuth.js')
+const adminRouter = require("./Routes/adminAuth.js");
 const path = require("path");
 const compile = require("./public/js/compile.js");
 const bodyParser = require("body-parser");
@@ -31,7 +31,7 @@ app.use(
 
 app.use("/StudentAuthentication", studentRouter);
 app.use("/quiz", quiz_solution);
-app.use('/admin',adminRouter);
+app.use("/admin", adminRouter);
 
 const checkUser = (req, res, next) => {
   if (req.session.usn == undefined) {
@@ -41,16 +41,12 @@ const checkUser = (req, res, next) => {
   }
 };
 
-
-
-
-
 app.get("/", (req, res) => {
   res.redirect("/home");
 });
-app.get('/loginDirect',(req,res)=>{
+app.get("/loginDirect", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "LoginDirect.html"));
-})
+});
 app.get("/home", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "home.html"));
 });
@@ -105,21 +101,75 @@ app.get("/networkutilities", checkUser, (req, res) => {
 app.get("/2nd", checkUser, (req, res) => {
   res.sendFile(path.join(__dirname, "views", "2ndsem.html"));
 });
-app.get("/cpl_lab1", checkUser, (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "cpl_lab1.html"));
+app.get("/5th", checkUser, (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "5sem.html"));
 });
-app.get("/cpl", checkUser, (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "cpl.html"));
+app.get("/cpl_labs", checkUser, (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "cpl_labs.html"));
 });
 app.get("/cpl", checkUser, (req, res) => {
   res.sendFile(path.join(__dirname, "views", "cpl.html"));
 });
 
+//Cn Labs routes
+
+app.get("/cn_lab1", checkUser, (req, res) => {
+  res.sendFile(path.join(__dirname, "views/cnlab", "cnlab1.html"));
+});
+app.get("/cn_lab2", checkUser, (req, res) => {
+  res.sendFile(path.join(__dirname, "views/cnlab", "cnlab2.html"));
+});
+app.get("/cn_lab3", checkUser, (req, res) => {
+  res.sendFile(path.join(__dirname, "views/cnlab", "cnlab3.html"));
+});
+app.get("/cn_lab4", checkUser, (req, res) => {
+  res.sendFile(path.join(__dirname, "views/cnlab", "cnlab4.html"));
+});
+app.get("/cn_lab5", checkUser, (req, res) => {
+  res.sendFile(path.join(__dirname, "views/cnlab", "cnlab5.html"));
+});
+app.get("/cn_lab6", checkUser, (req, res) => {
+  res.sendFile(path.join(__dirname, "views/cnlab", "cnlab6.html"));
+});
+app.get("/cn_lab7", checkUser, (req, res) => {
+  res.sendFile(path.join(__dirname, "views/cnlab", "cnlab7.html"));
+});
+app.get("/cn_lab8", checkUser, (req, res) => {
+  res.sendFile(path.join(__dirname, "views/cnlab", "cnlab8.html"));
+});
+app.get("/cn_lab9", checkUser, (req, res) => {
+  res.sendFile(path.join(__dirname, "views/cnlab", "cnlab9.html"));
+});
+app.get("/cn_lab10", checkUser, (req, res) => {
+  res.sendFile(path.join(__dirname, "views/cnlab", "cnlab10.html"));
+});
+app.get("/cn_lab11", checkUser, (req, res) => {
+  res.sendFile(path.join(__dirname, "views/cnlab", "cnlab11.html"));
+});
+app.get("/cn_lab12", checkUser, (req, res) => {
+  res.sendFile(path.join(__dirname, "views/cnlab", "cnlab12.html"));
+});
+app.get("/cn_lab13", checkUser, (req, res) => {
+  res.sendFile(path.join(__dirname, "views/cnlab", "cnlab13.html"));
+});
+app.get("/cn_lab14", checkUser, (req, res) => {
+  res.sendFile(path.join(__dirname, "views/cnlab", "cnlab14.html"));
+});
+app.get("/cn_lab15", checkUser, (req, res) => {
+  res.sendFile(path.join(__dirname, "views/cnlab", "cnlab15.html"));
+});
+app.get("/cn_lab16", checkUser, (req, res) => {
+  res.sendFile(path.join(__dirname, "views/cnlab", "cnlab16.html"));
+});
+app.get("/cn_lab17", checkUser, (req, res) => {
+  res.sendFile(path.join(__dirname, "views/cnlab", "cnlab17.html"));
+});
+app.get("/cn_lab18", checkUser, (req, res) => {
+  res.sendFile(path.join(__dirname, "views/cnlab", "cnlab18.html"));
+});
+
 var PORT = process.env.PORT || 3000;
-// app.listen( process.env.PORT || '3000', err => {
-//     if (err) console.log("OOPS!!server failed")
-//     else console.log("server started at port : 3000");
-// });
+
 app.listen(PORT, function () {
   console.log(
     "Express server listening on port %d in %s mode",
@@ -127,6 +177,3 @@ app.listen(PORT, function () {
     app.settings.env
   );
 });
-//    var PORT=process.env.PORT || '8080';
-
-// app.set("port",PORT);
---
+++ public/js/CplPrograms.js
@@ -0,0 +1,99 @@
+ProgramChoose = () => {
+  //   document.getElementById("cont").style.display = "none";
+
+  s = document.getElementById("cpl_lab1").value;
+
+  //   location.href = "";
+};
+
+// document.getElementById("demo").innerHTML = `
+// <div class="ui" id="cont">
+// <h2 class="ui header" style="font-size: 35px; margin-left: 10px;text-align: center;">
+//   Program to Demonstrate a Commercial Calculator
+// </h2>
+// <br>
+// <div class="ui stackable grid">
+//   <div class="four wide column">
+//     <div class="ui secondary vertical pointing menu" id="Menus">
+//       <a class="active item" id="intro"> Introduction </a>
+//       <a class="item" id="prereq"> Theory </a>
+//       <a class="item" href="/compile"  target="_blank"> Compiler </a>
+//       <a class="item" id="faq"> Quiz </a>
+//     </div>
+//   </div>
+//   <div class="twelve wide stretched column">
+//     <div class="ui segment">
+//       <div id="int">
+//         <br />
+//         <a
+//           href="https://drive.google.com/file/d/1EbX0y4n4vV2dqL6pvsSBYlENjAmiA6p7/view?usp=sharing"
+//           target="_blank"
+//           >Google Drive Link to Program 1
+//         </a>
+//         <br /><br />
+//         <!-- <iframe height="400px" width="100%" allowfullscreen="allowfullscreen" src="https://drive.google.com/file/d/1EbX0y4n4vV2dqL6pvsSBYlENjAmiA6p7/view?usp=sharing" frameborder="0"> -->
+//         <!-- </iframe> -->
+//         <br /><br />
+//         <!-- There are many real-life examples of a stack. Consider an example of plates stacked over one another in the canteen. The plate which is at the top is the first one to be removed, i.e. the plate which has been placed at the bottommost position remains in the stack for the longest period of time. So, it can be simply seen to follow LIFO(Last In First Out)/FILO(First In Last Out) order. -->
+//       </div>
+
+//       <div id="pre" style="display: none">
+
+//           <div  >
+//             <h1 style="color: rgb(15, 15, 109);">C switch Statement</h1>
+//             <p>           The switch statement allows us to execute one code block among many alternatives.
+
+//               You can do the same thing with the <span style="background-color:  rgb(196, 181, 181);"> if...else..if </span> ladder. However, the syntax of the <span style="background-color:  rgb(196, 181, 181);"> switch </span> statement is much easier to read and write.</p>
+
+//             </div>
+//             <br>
+//             <div class="ui fitted divider">
+//             </div>
+//             <div>
+//              <h2  style="color: rgb(15, 15, 109);"> Syntax of switch...case</h2>
+//              <h4>
+//               switch (expression) <br>
+//               ​{ <br>
+//                   case constant1: <br>
+//                     // statements <br>
+//                     break; <br>
+
+//                   case constant2: <br>
+//                     // statements <br>
+//                     break; <br>
+//                   . <br>
+//                   . <br>
+//                   . <br>
+//                   default: <br>
+//                     // default statements <br>
+//               } <br>
+//           </h4>
+//             </div>
+//             <br>
+//             <div class="ui fitted divider">
+//             </div>
+//             <br>
+//             <div>
+//                 <h2  style="color: rgb(15, 15, 109);">How does the switch statement work?</h2>
+
+//                 <p>The   <span style="background-color:  rgb(196, 181, 181);"> expression </span>is evaluated once and compared with the values of each <span style="background-color:  rgb(196, 181, 181);"> case </span> label.</p>
+//                 <div class="ui bulleted list">
+//                     <div class="item">
+//                         <p>If there is a match, the corresponding statements after the matching label are executed. For example, if the value of the expression is equal to <span style="background-color:  rgb(196, 181, 181);"> constant2 </span>, statements after  <span style="background-color:  rgb(196, 181, 181);">case constant2</span>: are executed until break<span style="background-color:  rgb(196, 181, 181);"> break </span> is encountered.</p>
+//                     </div>
+//                     <div class="item">
+//                         <p>If there is no match, the default statements are executed.</p>
+//                     </div>
+//                 </div>
+//                 <br>
+//                 <h3 style="color: rgb(15, 15, 109);">Switch Statement Flowchart</h3>
+//                 <img id="theoryImage" src="images/flowchart-switch-statement.jpg">
+//             </div>
+
+//           <div id="faqs" style="display: none"></div>
+//         </div>
+//       </div>
+//     </div>
+//   </div>
+// </div>
+// </div>`;
